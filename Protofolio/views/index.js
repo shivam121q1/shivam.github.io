4e39da8619fa6fc3b697dbaf1e07f2831b068879
shivam121q1 <105811692+shivam121q1@users.noreply.github.com>
2024-01-30 23:08:54 +0530

    Add files via upload

---
+++ Protofolio/views/index.js
@@ -0,0 +1,15 @@
+function social(id){
+    if(id == "ld"){
+        window.open("https://www.linkedin.com/in/shivam-pal-3b9b1b22b/","_blank")
+    }
+    if(id == "fb"){
+        window.open("https://www.linkedin.com/in/shivam-pal-3b9b1b22b/","_blank")
+    }
+    if(id == "gh"){
+        window.open("https://github.com/shivam121q1","_blank")
+    }
+    if(id == "ig"){
+        window.open("https://www.instagram.com/shivam.121.q/","_blank")
+    }
+    
+}
